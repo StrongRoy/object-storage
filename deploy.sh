42593535b2ccc7c4e77cd17f197aec830c4c75cf
wangliqiang <wangliqiang@rouchi.com>
2018-12-07 11:36:02 +0800

    update code

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
-for i in `seq 1 6`; do mkdir -p /tmp/$1objects;done
-export RABBITMQ_SERVER="amqp://test:test@172.16.114.133:5672"
+for i in `seq 1 6`; do mkdir -p /tmp/$i/objects;done
+export RABBITMQ_SERVER="amqp://test:test@localhost:5672"
 LISTEN_ADDRESS=172.16.1.1:12345 STORAGE_ROOT=/tmp1 ./bin/dataServer &
 LISTEN_ADDRESS=172.16.1.2:12345 STORAGE_ROOT=/tmp2 ./bin/dataServer &
 LISTEN_ADDRESS=172.16.1.3:12345 STORAGE_ROOT=/tmp3 ./bin/dataServer &
