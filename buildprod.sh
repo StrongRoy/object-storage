1a16299a629a8763557ab851b677b7124d0d326c
wangliqiang <wangliqiang@rouchi.com>
2018-12-07 11:01:30 +0800

    update bin

--- buildprod.sh
+++ buildprod.sh
@@ -3,10 +3,10 @@
 # Build web and other services
 
 cd  ~/go/src/richie.com/richie/object-storage/chapter2/apiServer
-env GOOS=linux GOARCH=amd64 go build -o ../bin/apiServer
+env GOOS=linux GOARCH=amd64 go build -o ../../bin/apiServer
 
 cd  ~/go/src/richie.com/richie/object-storage/chapter2/dataServer
-env GOOS=linux GOARCH=amd64 go build -o ../bin/dataServer
+env GOOS=linux GOARCH=amd64 go build -o ../../bin/dataServer
 
 
 
